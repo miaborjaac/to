811db7587116d17cd510fad454d3c14aa92c7363
Miguel Angel Borja <miaborjaac@unal.edu.co>
2017-08-23 20:45:23 +0000

    Añadido scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  resources :todos
   get 'welcome/contact'
 
   get 'welcome/about'
@@ -6,6 +8,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/about', to: 'welcome#about'
+  root 'welcome#about'
   get '/contact', to: 'welcome#contact'
   
 end
